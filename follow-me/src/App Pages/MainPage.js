1aa1235649689b9e6e525099307ceb2de163f763
aysellunlu <aysell.unlu48@gmail.com>
2022-09-19 17:34:16 +0300

    job list mapping

--- follow-me/src/App Pages/MainPage.js
+++ follow-me/src/App Pages/MainPage.js
@@ -1,16 +1,8 @@
-import React, { useState } from "react";
-import { Form, Button, Row, Col, Image, Modal } from "react-bootstrap";
-import DeleteImg from "../images/trash.png";
-import EditImg from "../images/edit.png";
+import React from "react";
 import NewJob from "../components/NewJob";
 import Table from "../components/Table";
 
 const MainPage = () => {
-  const [showEdit, setShowEdit] = useState(false);
-  const handleCloseEdit = () => setShowEdit(false);
-  const handleShowEdit = () => setShowEdit(true);
-
-
   return (
     <div className="container-fluid bg-color">
       <NewJob />
